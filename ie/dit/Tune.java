9f9d9b251e7d4e5882969676dedeeab17d5c1169
Aaron Sheehan <17aaronsheehan@gmail.com>
2018-02-14 16:39:47 +0000

    Tune Class

---
+++ ie/dit/Tune.java
@@ -0,0 +1,29 @@
+package ie.dit;
+
+public class Tune
+{
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+	
+	public int getX()
+	{
+		return x;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public String getAltTitle()
+	{
+		return altTitle;
+	}
+	
+	public String getNotation()
+	{
+		return notation;
+	}
+}
